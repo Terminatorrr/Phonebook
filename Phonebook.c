245461b01510b2afa1739d2abbc068ae758f0765
Terminatorrr <toha24112000@gmail.com>
2018-06-15 23:24:07 +0300

    Search works with any contact data (complete)

--- Phonebook.c
+++ Phonebook.c
@@ -12,17 +12,27 @@
 #include <string.h>
 #include <ctype.h>
 
+bool is_data_valid(string data, string data_type);
 string string_tolower(string z);
-bool contains(char a[], char b[]);
-bool validate_number(char n[]);
+bool is_name_valid(char a[]);
+bool is_number_valid(char n[]);
 void print_contact_info(int count);
-int get_name_index(string name);
+int find_contact_index(string data, string data_type);
+int find_name_index(string name);
+int find_number_index(string number);
 void edit_contact(int i);
+string get_contact_data();
+string get_data_type(string data);
 string get_contact_name();
 string get_contact_number();
+void add_contact(string name, string number);
 
 string phonebook[100][2];
 char invalid[] = "+*/=0123456789";
+string data_types_dictionary[] = {
+    {"name","0"},
+    {"number","1"}
+};
 int count = 0;
 
 int main (void)
@@ -30,39 +40,53 @@ int main (void)
     printf("Welcome to Phonebook!\n");
     while (true)
     {
-        string name = get_contact_name();
+        string data;
+        string data_type;
+        do
+        {
+            data = get_string("Enter contact data: ");
+            data_type = get_data_type(data);
+        }
+        while (!is_data_valid(data, data_type));
 
-        int name_index = get_name_index(name);
-        if (name_index > -1)
+        int contact_index = find_contact_index(data, data_type);
+        if (contact_index > -1)
         {
             printf("Contact found:\n");
-            print_contact_info(name_index);
-            edit_contact(name_index);
+            print_contact_info(contact_index);
+            edit_contact(contact_index);
             continue;
         }
-
-        string number = get_contact_number();
-
-        phonebook[count][0] = name;
-        phonebook[count][1] = number;
-        count++;
+        else
+        {
+            printf("%s not found. Creating new contact.\n", data);
+            if (data_type == "name") 
+            {
+                add_contact(data, NULL);
+            }
+            else
+            {
+                add_contact(NULL, data);
+            }  
+        }
     }
 }
 
-bool contains(char a[], char b[])
+bool is_name_valid(char a[])
 {
-    for (int i = 0; i < strlen(b); i++)
+    for (int i = 0; i < strlen(a); i++)
     {
-        if (strchr(a, b[i]) != NULL)
+        if (a[i] < 65 || a[i] > 90 && a[i] < 97 || a[i] > 122)
         {
-            return true;
+            printf("Name is invalid!\n");
+            return false;
         }
     }
 
-    return false;
+    return true;
 }
 
-bool validate_number(char n[])
+bool is_number_valid(char n[])
 {
     int j = strlen(n);
     if ((j >= 10 && j <= 15) && n[0] == '+')
@@ -71,6 +95,7 @@ bool validate_number(char n[])
         {
             if (n[i] < 48 || n[i] > 57)
             {
+                printf("Number is invalid!\n");
                 return false;
             }
         }
@@ -93,7 +118,19 @@ void print_contact_info(int position)
     printf("  %s\n  %s\n  #%i in the phonebook\n", contact_name, contact_number, position);
 }
 
-int get_name_index(string name)
+int find_contact_index(string data, string data_type)
+{
+    if (data_type == "name") 
+    {
+        return find_name_index(data);
+    }
+    else
+    {
+        return find_number_index(data);
+    }
+}
+
+int find_name_index(string name)
 {
     for (int i = 0, j = sizeof(phonebook); i < j; i++)
     {
@@ -102,6 +139,21 @@ int get_name_index(string name)
             return -1;
 
         if (strcmp(string_tolower(contact_name), string_tolower(name)) == 0)
+            return i;
+    }
+
+    return -1;
+}
+
+int find_number_index(string number)
+{
+    for (int i = 0, j = sizeof(phonebook); i < j; i++)
+    {
+        string contact_number = phonebook[i][1];
+        if (contact_number == NULL)
+            return -1;
+
+        if (strcmp(contact_number, number) == 0)
             return i;
     }
 
@@ -153,24 +205,82 @@ void edit_contact(int name_index)
     } 
 }
 
+string get_data_type(string data)
+{
+    return is_number_valid(data) ? "number" : "name";
+}
+
 string get_contact_name()
 {
     string name;
+    bool is_valid;
     do
     {
         name = get_string("Enter contact name: ");
+        is_valid = is_name_valid(name);
+        if (!is_valid)
+        {
+            printf("Invalid contact name.\n");
+        }
     }
-    while (contains(name, invalid));
+    while (!is_valid);
     return name;
 }
 
 string get_contact_number()
 {
     string number;
+    bool is_valid;
     do
     {
         number = get_string("Enter phone number: ");
+        is_valid = is_number_valid(number);
+        if (!is_valid)
+        {
+            printf("Invalid phone number.\n");
+        }
     }
-    while (!validate_number(number));
+    while (!is_valid);
     return number;
+}
+
+void start_add_contact(string data, string data_type)
+{
+    if (data_type == "name")
+    {
+        add_contact(data, NULL);
+    }
+    else
+    {
+        add_contact(NULL, data);
+    }
+}
+
+void add_contact(string name, string number)
+{
+    if (number == NULL || number == "" || !is_number_valid(number)) 
+    {
+        number = get_contact_number();
+    }
+
+    if (name == NULL || name == "" || !is_name_valid(name))
+    {
+        name = get_contact_name();
+    }
+
+    phonebook[count][0] = name;
+    phonebook[count][1] = number;
+
+    count++;
+    printf("New contact created.\n");
+}
+
+bool is_data_valid(string data, string data_type)
+{
+    if (data_type == "name")
+    {
+        return is_name_valid(data);
+    }
+    
+    return is_number_valid(data);
 }
